6de54b9c2aa716eec19be6734f4cbb1d5e7eacf6
Roylu7384 <roylu7384@gmail.com>
2022-03-22 15:21:39 -0400

    Render basic button and add button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
+// possible props
+// Confirm: Boolean;
+// Danger: Boolean;
+// Clickable:
+// Disabled: Boolean
+
+
 import React from "react";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   return (
+     <button>
+        {props.children}
+     </button> 
+   );
 }
