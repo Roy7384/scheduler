9db7325ec276f3637846c2f90538114a17648525
Roylu7384 <roylu7384@gmail.com>
2022-03-22 15:50:08 -0400

    Refactor with using classNames library to modify className

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,25 @@
-// possible props
+// possible props:
 // Confirm: Boolean;
 // Danger: Boolean;
 // Clickable:
 // Disabled: Boolean
 
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   };
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   };
+   let buttonClass = classNames("button", {
+      " button--confirm": props.confirm,
+      " button--danger": props.danger
+   });
 
    return (
-     <button onClick={props.onClick} disabled={props.disabled} className={buttonClass}>
+     <button 
+      className={buttonClass}
+      onClick={props.onClick} 
+      disabled={props.disabled} 
+      >
         {props.children}
      </button> 
    );
