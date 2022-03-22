a8b9108edbd93e980447e884ea2c69705b5b7cef
Roylu7384 <roylu7384@gmail.com>
2022-03-22 16:57:53 -0400

    Style DayListItem conditionally using different class names

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -3,12 +3,19 @@
 // selected:boolean true or false declaring that this day is selected
 // setDay:function accepts the name of the day eg. "monday", "tuesday"
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props;
 
+  const DayListItemClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": (!spots)
+  })
+
   return (
-    <li onClick={() => { setDay(name) }}>
+    <li className={DayListItemClass} onClick={() => { setDay(name) }}>
       <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
