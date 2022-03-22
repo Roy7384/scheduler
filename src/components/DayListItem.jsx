a27913f2c9ae01d75eb8720c37773c26a7bcc42d
Roylu7384 <roylu7384@gmail.com>
2022-03-22 16:16:56 -0400

    Get component to accept and use props

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,10 +1,16 @@
+// name:string the name of the day
+// spots:number the number of spots remaining
+// selected:boolean true or false declaring that this day is selected
+// setDay:function accepts the name of the day eg. "monday", "tuesday"
 import React from "react";
 
 export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => { setDay(name) }}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">{spots} spots remaining</h3>
     </li>
   );
 }
