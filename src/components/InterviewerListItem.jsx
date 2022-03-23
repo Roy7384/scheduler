282b4c62cf678ed518686187dca39d3815added8
Roylu7384 <roylu7384@gmail.com>
2022-03-23 13:35:56 -0400

    Refactor to take in props and show name conditionally

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+// props:
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected: Boolean
 
 export default function InterviewerListItem (props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const InterviewerListItemClassName = classNames("interviewers__item", {
+    "interviewers__item--selected": selected
+  })
 
   return (
-    <li className="interviewers__item">
+    <li onClick={() => {setInterviewer(id)}} className={InterviewerListItemClassName} key={id}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={avatar}
+        alt={name}
       />
-      Sylvia Palmer
+    {selected ? name : ''}
     </li>
   );
 };
