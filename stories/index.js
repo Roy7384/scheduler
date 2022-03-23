d0a1042a5e8517c996d497d772d7387d5a40b54f
Roylu7384 <roylu7384@gmail.com>
2022-03-23 16:32:41 -0400

    Setup Status component and story for it

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,4 +153,5 @@ storiesOf('Appoinment', module)
   .add('Empty', () => <Empty onAdd={action("onAdd")} />)
   .add('Show', () => <Show onEdit={action('onEdit')} onDelete={action('onDelete')} />)
   .add('Confirm', () => <Confirm onConfirm={action('onConfirm')} onCancel={action('onCancel')}/>)
+  .add('Status', () => <Status message={"Deleting"}/>)
 
