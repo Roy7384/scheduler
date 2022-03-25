8654d9873f9d6b04e03be6deaa19fd98eddc828f
Roylu7384 <roylu7384@gmail.com>
2022-03-25 18:35:55 -0400

    Use custome hook useVisualMode and change rendering logic to use state from it

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+
 import "./styles.scss";
+import useVisualMode from "hooks/useVisualMode";
+
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
@@ -8,10 +11,24 @@ export default function Appointment(props) {
 
   const { time, interview } = props;
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment">
       <Header time={time}/>
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer} 
+      />
+      )}
     </article>
   )
 }
