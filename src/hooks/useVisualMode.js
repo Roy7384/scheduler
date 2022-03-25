9e283476bfe02845089172b93e0cbca608d0104d
Roylu7384 <roylu7384@gmail.com>
2022-03-25 18:13:44 -0400

    Implement useVisualMode and pass tests about it

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useVisualMode(initMode) {
+  
+  const [mode, setMode] = useState(initMode);
+  const [history, setHistory] = useState([initMode]);
+
+  function transition(newMode, replace) {
+
+    if (replace) {
+      setMode(newMode);
+      const copyHistory = [...history];
+      copyHistory.pop();
+      setHistory([...copyHistory, newMode]);
+      return;
+    }
+    setMode(newMode)
+    setHistory([...history, newMode]); 
+  };
+  function back() {
+    if (history.length === 1) return;
+
+    setMode(history[history.length - 2]);
+    const copyHistory = [...history];
+    setHistory(copyHistory.slice(0, -1));
+  };
+
+  return { mode, transition, back };
+}
