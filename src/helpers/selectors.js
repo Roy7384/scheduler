477552cd4c8beb629b3476ee01a63be6752ad93e
Roylu7384 <roylu7384@gmail.com>
2022-03-25 14:02:45 -0400

    Implement getInterview selector function and pass tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,4 +17,9 @@ export function getAppointmentsForDay(state, day) {
 
 export function getInterview(state, interview) {
   
+  if (!interview) return null;
+  
+  const interviewerInfo = state.interviewers[interview.interviewer];
+
+  return {...interview, interviewer: interviewerInfo};
 }
