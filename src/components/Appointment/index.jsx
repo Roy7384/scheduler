f52aa89a7064e8a45ebaaf880860dbe9d334c3db
Roylu7384 <roylu7384@gmail.com>
2022-03-28 14:58:08 -0400

    Show Status component after clicking save and before database successfully update appointments

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -7,6 +7,7 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 
 export default function Appointment(props) {
 
@@ -15,16 +16,19 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
-  
+  const SAVING = "SAVING";
+
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
-  
+
   function save(name, interviewer) {
     const interview = {
       student: name,
       interviewer
-    }
+    };
+
+    transition(SAVING);
     bookInterview(id, interview).then(() => {
       transition(SHOW);
     });
@@ -32,21 +36,24 @@ export default function Appointment(props) {
 
   return (
     <article className="appointment">
-      <Header time={time}/>
+      <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={interview.student}
-          interviewer={interview.interviewer} 
-      />
+          interviewer={interview.interviewer}
+        />
       )}
       {mode === CREATE && (
-      <Form 
-        interviewers={interviewers}
-        onCancel={back}
-        onSave={save}
-      />
+        <Form
+          interviewers={interviewers}
+          onCancel={back}
+          onSave={save}
+        />
+      )}
+      {mode === SAVING && (
+        <Status />
       )}
     </article>
-  )
+  );
 }
