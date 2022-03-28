43af5e3e6479dc866eb8c7278f53427268030e35
Roylu7384 <roylu7384@gmail.com>
2022-03-28 15:17:29 -0400

    Update status message when saving

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -52,7 +52,7 @@ export default function Appointment(props) {
         />
       )}
       {mode === SAVING && (
-        <Status />
+        <Status message='Saving'/>
       )}
     </article>
   );
