77cd88ac04168b49c2b8688bdd8d7685defe9629
Roylu7384 <roylu7384@gmail.com>
2022-03-30 09:28:21 -0400

    Implement edit interview test and find a bug on updating spots

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, queryByText, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, getByTestId, getAllByTestId, getByAltText, queryByText, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -81,6 +81,38 @@ describe("Application", () => {
     const days = getAllByTestId(container, 'day');
     const monday = days.filter(day => queryByText(day, 'Monday'))[0]
     expect(getByText(monday, '2 spots remaining')).toBeInTheDocument(); 
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+  
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    // 3. Click the "Edit" button on the second appointment.
+    const secondAppointment = getAllByTestId(container, "appointment")[1]; 
+    fireEvent.click(getByAltText(secondAppointment, "Edit"));
+
+    // 4. Check the create Form is shown by checking if the save button is on screen.
+    expect(getByText(secondAppointment, 'Save')).toBeInTheDocument();
+
+    // 5. Change the student name to edit the appointment
+    fireEvent.change(getByTestId(secondAppointment, "student-name-input"), {target: {value: "Roy"}});
+
+    // 6. Click save button
+    fireEvent.click(getByText(secondAppointment, 'Save'))
+
+    // 7. Check that the element with the text "Saving" is displayed.
+    expect(getByText(secondAppointment, 'Saving')).toBeInTheDocument();
+    
+    // 8. Wait until the second appointment slot has shown different student name.
+    await waitForElement(() => getByText(secondAppointment, 'Roy'));
+
+    // 9. Check that the DayListItem with the text "Monday" still has the text "1 spot remaining".
+    const days = getAllByTestId(container, 'day');
+    const monday = days.filter(day => queryByText(day, 'Monday'))[0]
+    expect(getByText(monday, '1 spot remaining')).toBeInTheDocument(); 
   });
 });
 
