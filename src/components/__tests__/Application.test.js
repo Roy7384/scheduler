bd23ee57a087ef290c0bbb186f7f563db1f907ae
Roylu7384 <roylu7384@gmail.com>
2022-03-29 22:14:22 -0400

    Implement one complete integration test on Application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, queryByText, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -19,7 +19,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -37,8 +37,21 @@ describe("Application", () => {
 
     // click the save button
     fireEvent.click(getByText(appointment, "Save"));
+    
+    // checking if Status with test saving is showing
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
 
-    console.log(prettyDOM(appointment));
+    // wait until new appointment renders
+    await waitForElement(() => getByText(appointment, "Roy"));
+
+    // get all DayListItem
+    const days = getAllByTestId(container, "day");
+
+    // get monday item
+    const monday = days.filter(day => queryByText(day, 'Monday'))[0]
+
+    // check if monday has 'no spots remaining'
+    expect(getByText(monday, 'no spots remaining')).toBeInTheDocument();
   });
 
 });
