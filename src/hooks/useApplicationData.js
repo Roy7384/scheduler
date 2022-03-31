2e61f119e2641193bcd04519143ecafde19b8a70
Roylu7384 <roylu7384@gmail.com>
2022-03-30 21:24:07 -0400

    Clean up code a bit

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -5,7 +5,8 @@ import {
   SET_DAY,
   SET_APPLICATION_DATA,
   SET_INTERVIEW,
-  tasksReducer } from "reducers/application";
+  tasksReducer
+} from "reducers/application";
 
 export default function useApplicationData(props) {
 
@@ -23,99 +24,76 @@ export default function useApplicationData(props) {
     const daysUrl = '/api/days';
     const appointmentsUrl = '/api/appointments';
     const interviewersUrl = '/api/interviewers';
-    
+
     Promise.all([
       axios.get(daysUrl),
       axios.get(appointmentsUrl),
       axios.get(interviewersUrl)
     ]).then((all) => {
-      dispatch({ 
-        type: SET_APPLICATION_DATA, 
+      dispatch({
+        type: SET_APPLICATION_DATA,
         days: all[0].data,
         appointments: all[1].data,
         interviewers: all[2].data
       });
     });
-
-    // create WebSocket connection
-    const wsUrl = process.env.REACT_APP_WEBSOCKET_URL;
-    const webSocket = new WebSocket(wsUrl);
-
-    webSocket.onopen = function(event) {
-      webSocket.send('ping');
-    }
-    // webSocket.onmessage = event => { 
-    //   if (event.data.type === SET_INTERVIEW) {
-    //     const { id, interview } = event.data
-    //     const appointment = {
-    //       ...state.appointments[id],
-    //       interview: { ...interview }
-    //     };
-    //     const appointments = {
-    //       ...state.appointments,
-    //       [id]: appointment
-    //     };
-    //     const days = updateSpots()
-    //     dispatch({ type: SET_INTERVIEW, appointments, days})
-    //   }
-    // }
   }, []);
 
-  // function to update day selected by user
-  const setDay = day => dispatch({ type: SET_DAY, day });
+// function to update day selected by user
+const setDay = day => dispatch({ type: SET_DAY, day });
 
-  // function to calculate spots remaining and update it in state
-  function updateSpots(newAppointments) {
-  
-    // find which day the appointment belong to
-    const dayToUpdate = state.days.find(day => day.name === state.day);
-    
-    // calculate spots by looping through appointments
-    let spots = 0;
-    dayToUpdate.appointments.forEach(appId => {
-      if (!newAppointments[appId].interview) {
-        spots ++
-      }
-    })
-    const newDay = {...dayToUpdate, spots};
+// function to calculate spots remaining and update it in state
+function updateSpots(newAppointments) {
 
-    // Return new days state
-    return state.days.map(day => day.name === state.day ? newDay : day);
-  }
+  // find which day the appointment belong to
+  const dayToUpdate = state.days.find(d => d.name === state.day);
 
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+  // calculate spots by looping through appointments
+  let spots = 0;
+  dayToUpdate.appointments.forEach(appId => {
+    if (!newAppointments[appId].interview) {
+      spots++;
+    }
+  });
+  const newDay = { ...dayToUpdate, spots };
 
-    return axios.put(`/api/appointments/${id}`, appointment)
-      .then(() => {
-        const days = updateSpots(appointments);
-        dispatch({ type: SET_INTERVIEW, appointments, days });
-      });
-  }
+  // Return new days state
+  return state.days.map(d => d.name === state.day ? newDay : d);
+}
 
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+function bookInterview(id, interview) {
+  const appointment = {
+    ...state.appointments[id],
+    interview: { ...interview }
+  };
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment
+  };
 
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        const days = updateSpots(appointments);
-        dispatch({ type: SET_INTERVIEW, appointments, days });
-      });
-  }
+  return axios.put(`/api/appointments/${id}`, appointment)
+    .then(() => {
+      const days = updateSpots(appointments);
+      dispatch({ type: SET_INTERVIEW, appointments, days });
+    });
+}
 
-  return { state, setDay, bookInterview, cancelInterview };
+function cancelInterview(id) {
+  const appointment = {
+    ...state.appointments[id],
+    interview: null
+  };
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment
+  };
+
+  return axios.delete(`/api/appointments/${id}`)
+    .then(() => {
+      const days = updateSpots(appointments);
+      dispatch({ type: SET_INTERVIEW, appointments, days });
+    });
+}
+
+return { state, setDay, bookInterview, cancelInterview };
 }
