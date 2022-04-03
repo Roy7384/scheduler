8360979875163c1d656058726409eb891ac0eb0c
Roylu7384 <roylu7384@gmail.com>
2022-04-03 17:12:08 -0400

    Update base url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Application from "components/Application";
 
 import axios from "axios";
 
-if (process.env.REACT_APP_BASE_URL) {
+if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 }
 
